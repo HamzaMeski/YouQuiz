c950ecf424d8d7108f3cc02fee744c70a1b2215c
HamzaMeski <meskihamza5@gmail.com>
2024-12-04 10:39:50 +0100

    fixing assignemmnt

--- backend/src/main/java/com/youquiz/backend/components/quizAssignment/dto/response/AssignmentResponseDTO.java
+++ backend/src/main/java/com/youquiz/backend/components/quizAssignment/dto/response/AssignmentResponseDTO.java
@@ -1,10 +1,9 @@
 package com.youquiz.backend.components.quizAssignment.dto.response;
 
-import com.youquiz.backend.EntityComponentsProvider.dto.request.CreateDTO;
 import com.youquiz.backend.EntityComponentsProvider.dto.response.ResponseDTO;
-import com.youquiz.backend.entities.Quiz;
+import com.youquiz.backend.components.quiz.dto.response.QuizResponseDTO;
+import com.youquiz.backend.components.student.dto.response.StudentResponseDTO;
 import com.youquiz.backend.entities.QuizAssignment;
-import com.youquiz.backend.entities.Student;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -18,8 +17,8 @@ import java.time.LocalTime;
 @EqualsAndHashCode(callSuper = true)
 public class AssignmentResponseDTO extends ResponseDTO<QuizAssignment, Long> {
     private Long id;
-    private Quiz quiz;
-    private Student student;
+    private QuizResponseDTO quiz;
+    private StudentResponseDTO student;
     private String reason;
     private LocalTime startTime;
     private LocalTime endTime;
--- backend/src/main/java/com/youquiz/backend/components/quizAssignment/mapper/QuizAssingmentMapper.java
+++ backend/src/main/java/com/youquiz/backend/components/quizAssignment/mapper/QuizAssingmentMapper.java
@@ -1,26 +1,30 @@
 package com.youquiz.backend.components.quizAssignment.mapper;
 
 import com.youquiz.backend.EntityComponentsProvider.mapper.EntityMapper;
+import com.youquiz.backend.components.quiz.mapper.QuizMapper;
 import com.youquiz.backend.components.quizAssignment.dto.request.CreateAssignmenentDTO;
 import com.youquiz.backend.components.quizAssignment.dto.request.UpdateAssignmenentDTO;
 import com.youquiz.backend.components.quizAssignment.dto.response.AssignmentResponseDTO;
+import com.youquiz.backend.components.student.mapper.StudentMapper;
 import com.youquiz.backend.entities.QuizAssignment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {QuizMapper.class, StudentMapper.class})
 public interface QuizAssingmentMapper extends EntityMapper<QuizAssignment, Long, CreateAssignmenentDTO, UpdateAssignmenentDTO, AssignmentResponseDTO>  {
     @Override
-    @Mapping(target ="student", ignore = true)
+    @Mapping(target = "student", ignore = true)
     @Mapping(target = "quiz", ignore = true)
     QuizAssignment toEntity(CreateAssignmenentDTO createAssignmenentDTO);
 
     @Override
-    @Mapping(target ="student", ignore = true)
+    @Mapping(target = "student", ignore = true)
     @Mapping(target = "quiz", ignore = true)
     void updateEntity(UpdateAssignmenentDTO updateAssignmenentDTO, @MappingTarget QuizAssignment quizAssignment);
 
     @Override
+    @Mapping(target = "quiz", source = "quiz")
+    @Mapping(target = "student", source = "student")
     AssignmentResponseDTO toResponseDTO(QuizAssignment quizAssignment);
 }
